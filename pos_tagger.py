ea4905fb9ec8578d2e1fdcf0a840926961424cc8
Haofan Yang <haofanyoung@gmail.com>
2020-04-10 18:08:18 -0400

    O(n) Complexity Implementation

--- pos_tagger.py
+++ pos_tagger.py
@@ -25,12 +25,101 @@ class POSTagger():
     log(P(the current word is word|the current tag is POS))
     '''
     def train(self, train_set):
-        # iterate over training documents
+        # Construct index dicts
         for root, dirs, files in os.walk(train_set):
             for name in files:
                 with open(os.path.join(root, name)) as f:
-                    # be sure to split documents into sentences here
-                    pass
+                    # Split the document into sentences
+                    lines = f.readlines()
+                for line in lines:
+                    tagged_tokens = line.split()
+                    for pair in tagged_tokens:
+                        # Split the pair by the last occurence of '/'
+                        token, tag = pair.rsplit('/', 1)
+                        # Add indices for the current token and tag, if has not been added before
+                        if not token in self.word_dict:
+                            self.word_dict[token] = len(self.word_dict)
+                        if not tag in self.pos_dict:
+                            self.pos_dict[tag] = len(self.pos_dict)
+        self.word_dict[self.UNK] = len(self.word_dict) # Add UNK
+        # Initialize counters
+        num_of_tags, num_of_tokens = len(self.pos_dict), len(self.word_dict)
+        self.initial = np.zeros([num_of_tags])
+        self.transition = np.zeros([num_of_tags, num_of_tags])
+        self.emission = np.zeros([num_of_tags, num_of_tokens])
+        # Construct counters
+        for root, dirs, files in os.walk(train_set):
+            for name in files:
+                with open(os.path.join(root, name)) as f:
+                    lines = f.readlines()
+                for line in lines:
+                    tagged_tokens = line.split()
+                    prev_tag = None
+                    for pair in tagged_tokens:
+                        token, tag = pair.rsplit('/', 1)
+                        # Count emission, based on the pair
+                        cur_tag_ind = self.pos_dict[tag]
+                        cur_token_ind = self.word_dict[token]
+                        self.emission[cur_tag_ind][cur_token_ind] += 1
+                        # If prev is None, add to self.initial. Otherwise Add to self.transition, based on the previous tag.
+                        if prev_tag is None:
+                            self.initial[cur_tag_ind] += 1
+                        else:
+                            prev_tag_ind = self.pos_dict[prev_tag]
+                            self.transition[prev_tag_ind][cur_tag_ind] += 1
+                        prev_tag = tag
+        # Set all unk-related count to 1
+        for row in self.emission:
+            row[self.word_dict[self.UNK]] = 1
+        # Add-k smoothing
+        self.initial += 1
+        self.emission += 1
+        self.transition += 1
+        # Compute probabilities 
+        self.initial = self.initial / self.initial.sum()
+        self.emission = self.emission / self.emission.sum(axis = 1, keepdims = True)
+        self.transition = self.transition / self.transition.sum(axis = 1, keepdims = True)
+        # Validate probabilities sum to one
+        self.__validate_probabilities()
+
+                        # Add indices for the current token and tag, if has not been added before
+                        # When a new word/tag appears, we also need to add a new row/col to matrices
+                        if not token in self.word_dict:
+                            self.word_dict[token] = len(self.word_dict)
+                            self.emission = np.append(self.emission, np.zeros([self.emission.shape[0], 1]), axis = 1) # Add a new col to emission
+                        if not tag in self.pos_dict:
+                            self.pos_dict[tag] = len(self.pos_dict)
+                            self.initial = np.append(self.initial, 0) # Added a place holder for new tag
+                            # Add a new col and row in transition
+                            self.transition = np.append(self.transition, np.zeros([1, self.transition.shape[1]]), axis = 0)
+                            self.transition = np.append(self.transition, np.zeros([self.transition.shape[0], 1]), axis = 1)
+                            self.emission = np.append(self.emission, np.zeros([1, self.emission.shape[1]]), axis = 0) # Add a new row in emission
+                        
+                        # Count emission, based on the pair
+                        cur_tag_ind = self.pos_dict[tag]
+                        cur_token_ind = self.word_dict[token]
+                        self.emission[cur_tag_ind][cur_token_ind] += 1
+
+                        # If prev is None, add to initial. Otherwise Add to Transition, based on the previous tag.
+                        if prev_tag is None:
+                            self.initial[cur_tag_ind] += 1
+                        else:
+                            prev_tag_ind = self.pos_dict[prev_tag]
+                            self.transition[prev_tag_ind][cur_tag_ind] += 1
+                        prev_tag = tag
+        # Add a col for <UNK> in self.emission
+        new_col = np.ones([self.emission.shape[0], 1])
+        self.emission = np.append(self.emission, new_col, axis = 1)
+        # Add-k smoothing
+        self.initial += 1
+        self.emission += 1
+        self.transition += 1
+        # Compute probabilities 
+        self.initial = self.initial / self.initial.sum()
+        self.emission = self.emission / self.emission.sum(axis = 1, keepdims = True)
+        self.transition = self.transition / self.transition.sum(axis = 1, keepdims = True)
+        # Validate probabilities sum to one
+        self.__validate_probabilities()
 
     '''
     Implements the Viterbi algorithm.
@@ -68,9 +157,22 @@ class POSTagger():
         accuracy = 0.0
         return accuracy
 
+    def __validate_probabilities(self):
+        tor = 1.e-10
+        if abs(self.initial.sum() - 1) > tor:
+            print("Initial probabilities do not sum to one")
+        for row in self.emission:
+            if abs(row.sum() - 1) > tor:
+                print("Emission probabilities do not sum to one")
+        for row in self.transition:
+            if abs(row.sum() - 1) > tor:
+                print("Transition probabilities do not sum to one")
+
 if __name__ == '__main__':
     pos = POSTagger()
     # make sure these point to the right directories
-    pos.train('brown/train')
-    results = pos.test('brown/dev')
+    pos.train('train')
+    results = pos.test('dev')
+    # pos.train('train_small')
+    # results = pos.test('test_small')
     print('Accuracy:', pos.evaluate(results))
